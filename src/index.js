d7f2b730591b40120b453d7ddbdac3a6dca9192e
ctang415 <carrietang117@gmail.com>
2022-11-22 18:49:51 -0500

    Created basic form template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+import countries from "countries-list";
+
+  const countryCodes = Object.keys(countries.countries);
+  const countryNames = countryCodes.map(code => countries.countries[code].name);
+  const sortCountry = countryNames.sort();
+  sortCountry.forEach(element => {
+    const countrySelection = document.createElement('option');
+    countrySelection.value = element;
+    countrySelection.textContent = element;
+    document.getElementById('country').appendChild(countrySelection)
+  })
