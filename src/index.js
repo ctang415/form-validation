848eae8675b81a10770c5c7b6b36a6695173b800
ctang415 <carrietang117@gmail.com>
2022-11-23 14:38:24 -0500

    Added zipcode library to require proper zipcode patterns

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import './style.css';
 import countries from "countries-list";
+import zipit from "zip-it";
 
   const countryCodes = Object.keys(countries.countries);
-  const countryNames = countryCodes.map(code => countries.countries[code].name);
-  console.log(countryNames)
-  const sortCountry = countryNames.sort();
-  console.log(countryCodes)
-  console.log(sortCountry)
-  sortCountry.forEach(element => {
+  countryCodes.forEach(element => {
     const countrySelection = document.createElement('option');
     countrySelection.value = element;
     countrySelection.textContent = element;
-    document.getElementById('country').appendChild(countrySelection)
+    document.getElementById('country').appendChild(countrySelection);
   })
 
+  const countrySelected = document.getElementById('country');
+  countrySelected.addEventListener('change', function() {
+    const countryZipcode = document.getElementById('zipcode');
+    let zipValue = this.value;
+    let zipCodePattern = zipit.getRegexForCountry(zipValue);
+    countryZipcode.setAttribute('pattern', zipCodePattern);
+    console.log(countryZipcode.pattern);
+  })
