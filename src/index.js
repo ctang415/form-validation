228a8713df38282feca9e2336ec8faa80c18ae5a
ctang415 <carrietang117@gmail.com>
2022-11-22 22:02:44 -0500

    Aligned inputs on form

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import countries from "countries-list";
 
   const countryCodes = Object.keys(countries.countries);
   const countryNames = countryCodes.map(code => countries.countries[code].name);
+  console.log(countryNames)
   const sortCountry = countryNames.sort();
+  console.log(countryCodes)
+  console.log(sortCountry)
   sortCountry.forEach(element => {
     const countrySelection = document.createElement('option');
     countrySelection.value = element;
     countrySelection.textContent = element;
     document.getElementById('country').appendChild(countrySelection)
   })
+
